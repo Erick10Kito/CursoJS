d4a22aeec1ed0ee4ebd0549bd81cfd716fea352e
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-08-04 09:17:06 -0300

    Sobre os tipos de console.log que são aceitos

---
+++ aula3/ex/exerciciosDeConsole.js
@@ -0,0 +1,5 @@
+console.log(
+  'Meu nome é "Erick de Bem Campos". Estou aprendendo JavaScript às',
+  8,
+  "da manhâ."
+);
---
+++ aula3/tiposDeConsole.js
@@ -0,0 +1,8 @@
+//TIPOS DE CONSOLE ACEITOS(lembrando que não são todos, são somete alguns)
+
+console.log("OLA"); // Esse console é somente um OLA entre aspas duplas, normal, tambem é permitido que ele esteja entre aspas simples.
+console.log(123456); // Esse console é um numero, ou seja quando envolve numeros não é preciso usar aspas nenhuma.
+console.log("OLA'MUNDO'"); // Esse é um console com aspas duplas por fora e aspas simples por dentro do texto, não pode ser usado aspas duplas duas vezes pelo fato de confundir o javascript.
+console.log('OLA"MUNDO"'); // Esse é um exemplo do contrario do de cima, aspas simples por fora e dupla por dentro, tambem para não confundir o console do javascript.
+console.log(`"OLA"'MUNDO'`); //Esse é o unico metodo em que pode ser usado os dois tipos de aspas, pois tem a crase em volta de tudo, ai não confunde o console.
+console.log(35, 25, "OLA MUNDO", 1); // Esse é o metodo de separação por virgula, que ai os itens do console ficam na mesma linha.

5093f8fd79d2b6a379dd2077cabed12541e23c17
Erick de Bem Campos <erickcamposbem69@gmail.com>
2023-08-08 09:00:42 -0300

    Dados Primitivos e LetXVar

---
+++ DadosPrimitivos/index.js
@@ -0,0 +1,8 @@
+const nome = "Erick"; // string
+const nome1 = "Erick"; // string
+const nome2 = `Erick`; //string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta para local nenhum na memoria
+const sobremoneAluno = null; // Nulo -> não aponta para local nenhum na memoria
+const aprovado = false; // Boolean = true, false (lógico)
---
+++ VarXLet/index.js
@@ -0,0 +1,7 @@
+var nome = "Erick";
+var nome = "Campos"; // Com var é possivel redeclarar variaveis
+
+nome = "Erick"; // NÃO FAÇA ISSO
+
+let nome = "Erick";
+nome = "Campos"; // Em Let não é possivel redeclarar uma variavel, por isso usamos dessa forma para trocar o valor dela
